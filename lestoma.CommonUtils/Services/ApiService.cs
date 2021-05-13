567d5392cad3167980dc61c79905bfd65e1af1cc
diegoparra <48141200+DiegoArturoParra@users.noreply.github.com>
2021-05-12 19:01:46 -0500

    mostrando mensajes y cambiando a http la api ya que el ssl no es seguro y no consume en el emulador.

--- lestoma.CommonUtils/Services/ApiService.cs
+++ lestoma.CommonUtils/Services/ApiService.cs
@@ -65,7 +65,11 @@ namespace lestoma.CommonUtils.Services
                 Response respuesta = JsonConvert.DeserializeObject<Response>(jsonString);
                 if (!response.IsSuccessStatusCode)
                 {
-                    respuesta.IsExito = false;
+                    return new Response
+                    {
+                        IsExito = false,
+                        Mensaje = mostrarMensajePersonalizadoStatus(response.StatusCode)
+                    };
                 }
                 return respuesta;
             }
@@ -79,5 +83,100 @@ namespace lestoma.CommonUtils.Services
             }
         }
         #endregion
+
+
+        private string mostrarMensajePersonalizadoStatus(System.Net.HttpStatusCode statusCode)
+        {
+            string mensaje = string.Empty;
+            switch (statusCode)
+            {
+                case System.Net.HttpStatusCode.Accepted:
+                    break;
+                case System.Net.HttpStatusCode.Ambiguous:
+                    break;
+                case System.Net.HttpStatusCode.BadGateway:
+                    break;
+                case System.Net.HttpStatusCode.BadRequest:
+                    break;
+                case System.Net.HttpStatusCode.Conflict:
+                    break;
+                case System.Net.HttpStatusCode.Continue:
+                    break;
+                case System.Net.HttpStatusCode.Created:
+                    break;
+                case System.Net.HttpStatusCode.ExpectationFailed:
+                    break;
+                case System.Net.HttpStatusCode.Forbidden:
+                    break;
+                case System.Net.HttpStatusCode.Found:
+                    break;
+                case System.Net.HttpStatusCode.GatewayTimeout:
+                    break;
+                case System.Net.HttpStatusCode.Gone:
+                    break;
+                case System.Net.HttpStatusCode.HttpVersionNotSupported:
+                    break;
+                case System.Net.HttpStatusCode.InternalServerError:
+                    break;
+                case System.Net.HttpStatusCode.LengthRequired:
+                    break;
+                case System.Net.HttpStatusCode.MethodNotAllowed:
+                    break;
+                case System.Net.HttpStatusCode.Moved:
+                    break;
+                case System.Net.HttpStatusCode.NoContent:
+                    break;
+                case System.Net.HttpStatusCode.NonAuthoritativeInformation:
+                    break;
+                case System.Net.HttpStatusCode.NotAcceptable:
+                    break;
+                case System.Net.HttpStatusCode.NotFound:
+                    mensaje = "Petición no encontrada.";
+                    break;
+                case System.Net.HttpStatusCode.NotImplemented:
+                    break;
+                case System.Net.HttpStatusCode.NotModified:
+                    break;
+                case System.Net.HttpStatusCode.OK:
+                    break;
+                case System.Net.HttpStatusCode.PartialContent:
+                    break;
+                case System.Net.HttpStatusCode.PaymentRequired:
+                    break;
+                case System.Net.HttpStatusCode.PreconditionFailed:
+                    break;
+                case System.Net.HttpStatusCode.ProxyAuthenticationRequired:
+                    break;
+                case System.Net.HttpStatusCode.RedirectKeepVerb:
+                    break;
+                case System.Net.HttpStatusCode.RedirectMethod:
+                    break;
+                case System.Net.HttpStatusCode.RequestedRangeNotSatisfiable:
+                    break;
+                case System.Net.HttpStatusCode.RequestEntityTooLarge:
+                    break;
+                case System.Net.HttpStatusCode.RequestTimeout:
+                    break;
+                case System.Net.HttpStatusCode.RequestUriTooLong:
+                    break;
+                case System.Net.HttpStatusCode.ResetContent:
+                    break;
+                case System.Net.HttpStatusCode.ServiceUnavailable:
+                    break;
+                case System.Net.HttpStatusCode.SwitchingProtocols:
+                    break;
+                case System.Net.HttpStatusCode.Unauthorized:
+                    break;
+                case System.Net.HttpStatusCode.UnsupportedMediaType:
+                    break;
+                case System.Net.HttpStatusCode.Unused:
+                    break;
+                case System.Net.HttpStatusCode.UpgradeRequired:
+                    break;
+                case System.Net.HttpStatusCode.UseProxy:
+                    break;
+            }
+            return mensaje;
+        }
     }
 }
