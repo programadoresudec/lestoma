a7cd0bb44eca4686caee65edc85605b6ab968bee
Diego Arturo Parra Molina <48141200+DiegoArturoParra@users.noreply.github.com>
2023-02-09 19:07:41 -0500

    @Anderson0816  activar y desactivar notificaciones via email
    @Anderson0816

--- lestoma.App/lestoma.App/ViewModels/Account/SettingsViewModel.cs
+++ lestoma.App/lestoma.App/ViewModels/Account/SettingsViewModel.cs
@@ -1,5 +1,11 @@
-﻿using lestoma.App.Views.Account;
+﻿using Acr.UserDialogs;
+using lestoma.App.Views;
+using lestoma.App.Views.Account;
+using lestoma.CommonUtils.DTOs;
+using lestoma.CommonUtils.Interfaces;
 using Prism.Navigation;
+using Rg.Plugins.Popup.Services;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Internals;
 
@@ -9,17 +15,20 @@ namespace lestoma.App.ViewModels.Account
     public class SettingsViewModel : BaseViewModel
     {
         private readonly INavigationService _navigationService;
+        private readonly IApiService _apiService;
         private string _fullName;
         #region Constructor
 
-        public SettingsViewModel(INavigationService navigationService)
+        public SettingsViewModel(INavigationService navigationService, IApiService apiService)
             : base(navigationService)
         {
             _navigationService = navigationService;
-            this.EditProfileCommand = new Command(this.EditProfileClicked);
-            this.ChangePasswordCommand = new Command(this.ChangePasswordClicked);
-            this.HelpCommand = new Command(this.HelpClicked);
-            this.LogoutCommand = new Command(this.LogoutClicked);
+            _apiService = apiService;
+            EditProfileCommand = new Command(EditProfileClicked);
+            ChangePasswordCommand = new Command(ChangePasswordClicked);
+            HelpCommand = new Command(HelpClicked);
+            LogoutCommand = new Command(LogoutClicked);
+            StateChangedCommand = new Command<object>(SwitchStateChanged, CanNavigate);
             _fullName = TokenUser.User.FullName;
         }
         #endregion
@@ -37,9 +46,14 @@ namespace lestoma.App.ViewModels.Account
         public Command ChangePasswordCommand { get; set; }
         public Command HelpCommand { get; set; }
         public Command LogoutCommand { get; set; }
+        public Command StateChangedCommand { get; set; }
         #endregion
 
         #region Methods
+        private bool CanNavigate(object arg)
+        {
+            return true;
+        }
         private void EditProfileClicked(object obj)
         {
             // Do something
@@ -61,5 +75,82 @@ namespace lestoma.App.ViewModels.Account
         }
 
         #endregion
+        private void SwitchStateChanged(object obj)
+        {
+            var SwitchState = obj as Syncfusion.XForms.Buttons.SwitchStateChangedEventArgs;
+            if (SwitchState.NewValue.Value)
+            {
+                ActivateNotificationsViaEmail();
+            }
+            else
+            {
+                DesactivateNotificationsViaEmail();
+
+            }
+        }
+        private async void ActivateNotificationsViaEmail()
+        {
+            try
+            {
+                UserDialogs.Instance.ShowLoading("Activando...");
+                if (_apiService.CheckConnection())
+                {
+                    ResponseDTO response = await _apiService.PostWithoutBodyAsyncWithToken(URL_API,
+                   $"Account/enable-notifications-by-mail", TokenUser.Token);
+                    if (response.IsExito)
+                    {
+                        await PopupNavigation.Instance.PushAsync(new MessagePopupPage(response.MensajeHttp));
+                    }
+                    else
+                    {
+                        AlertWarning(response.MensajeHttp);
+                    }
+                }
+                else
+                {
+                    AlertNoInternetConnection();
+                }
+            }
+            catch (Exception ex)
+            {
+                SeeError(ex);
+            }
+            finally
+            {
+                UserDialogs.Instance.HideLoading();
+            }
+        }
+        private async void DesactivateNotificationsViaEmail()
+        {
+            try
+            {
+                UserDialogs.Instance.ShowLoading("Desactivando...");
+                if (_apiService.CheckConnection())
+                {
+                    ResponseDTO response = await _apiService.PostWithoutBodyAsyncWithToken(URL_API,
+                   $"Account/disable-notifications-by-mail", TokenUser.Token);
+                    if (response.IsExito)
+                    {
+                        await PopupNavigation.Instance.PushAsync(new MessagePopupPage(response.MensajeHttp));
+                    }
+                    else
+                    {
+                        AlertWarning(response.MensajeHttp);
+                    }
+                }
+                else
+                {
+                    AlertNoInternetConnection();
+                }
+            }
+            catch (Exception ex)
+            {
+                SeeError(ex);
+            }
+            finally
+            {
+                UserDialogs.Instance.HideLoading();
+            }
+        }
     }
 }
