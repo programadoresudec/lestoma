98701f44b4084c85b2abb7d75dd4fe3f8310635c
Diego Arturo Parra Molina <diegop177@hotmail.com>
2023-02-28 22:49:28 -0500

    @Anderson0816
    Class offline of component and laboratory

--- lestoma.DatabaseOffline/ModelsOffline/LaboratorioOffline.cs
+++ lestoma.DatabaseOffline/ModelsOffline/LaboratorioOffline.cs
@@ -1,10 +1,30 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using lestoma.Entidades.Models;
+using System.ComponentModel.DataAnnotations;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace lestoma.DatabaseOffline.ModelsOffline
 {
     public class LaboratorioOffline
     {
-        [Column("fecha_creacion_dispositivo")]
-        public string FechaCreacionDispositivo { get; set; }
+        [Key]
+        public Guid Id { get; set; }
+        [Required(ErrorMessage = "Campo requerido.")]
+        public Guid ComponenteLaboratorioId { get; set; }
+        public double? ValorCalculadoTramaEnviada { get; set; }
+        public double? ValorCalculadoTramaRecibida { get; set; }
+        public string TramaEnviada { get; set; }
+        public string TramaRecibida { get; set; }
+        public bool EstadoInternet { get; set; }
+        [Required(ErrorMessage = "Campo requerido.")]
+        public string Ip { get; set; }
+        [Required(ErrorMessage = "Campo requerido.")]
+        public string Session { get; set; }
+        [Required(ErrorMessage = "Campo requerido.")]
+        public string TipoDeAplicacion { get; set; }
+        [Required(ErrorMessage = "Campo requerido.")]
+        public DateTime FechaCreacionServer { get; set; }
+        [Required(ErrorMessage = "Campo requerido.")]
+        public DateTime FechaCreacionDispositivo { get; set; }
     }
 }
