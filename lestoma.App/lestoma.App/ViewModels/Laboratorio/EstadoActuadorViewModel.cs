ea50ac5215884e6eacd622f07b366a6d902e86d3
Diego Arturo Parra Molina <48141200+DiegoArturoParra@users.noreply.github.com>
2023-03-07 17:36:22 -0500

    vistas de la trama en los diferentes estados

---
+++ lestoma.App/lestoma.App/ViewModels/Laboratorio/EstadoActuadorViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace lestoma.App.ViewModels.Laboratorio
+{
+    public class EstadoActuadorViewModel : BaseViewModel
+    {
+        public EstadoActuadorViewModel(INavigationService navigationService) : base(navigationService)
+        {
+
+        }
+    }
+}
---
+++ lestoma.App/lestoma.App/Views/Laboratorio/SetPointPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace lestoma.App.Views.Laboratorio
+{
+    public partial class SetPointPage : ContentPage
+    {
+        public SetPointPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
